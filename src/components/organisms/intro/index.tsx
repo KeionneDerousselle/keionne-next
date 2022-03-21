d26b1ba425fc8e323df4488864085b663c708f9f
Keionne Derousselle <knnderousselle@gmail.com>
2022-03-20 20:08:03 -0600

    fixed navigation by removing scroll libraries

--- src/components/organisms/intro/index.tsx
+++ src/components/organisms/intro/index.tsx
@@ -1,6 +1,6 @@
 import { FC, useLayoutEffect } from 'react';
 import ParallaxJS from 'parallax-js';
-import { Link } from 'react-scroll';
+import NextLink from 'next/link';
 import classNames from 'classnames';
 
 const Parallax: FC = () => {
@@ -190,19 +190,14 @@ const Parallax: FC = () => {
 
 const ScrollDown: FC = () => (
   <div className="absolute left-0 z-20 w-full bottom-10 scrolldown">
-    <Link
-      to="about"
-      className="flex flex-col items-center m-auto text-sm text-center text-white cursor-pointer max-w-[100px]"
-      spy
-      hashSpy
-      smooth
-      offset={-112}
-    >
-      <span>Scroll Down</span>
-      <span className="relative w-5 border-2 border-white scrolldown__mouse rounded-2xl mt-[10px] h-[26px]">
-        <span className="absolute block w-1 h-1 bg-white rounded-full scrolldown__mouse__wheel top-2 left-1/2 -translate-x-1/2 animate-mouse-wheel" />
-      </span>
-    </Link>
+    <NextLink href="/#about" passHref>
+      <a className="flex flex-col items-center m-auto text-sm text-center text-white cursor-pointer max-w-[100px]">
+        <span>Scroll Down</span>
+        <span className="relative w-5 border-2 border-white scrolldown__mouse rounded-2xl mt-[10px] h-[26px]">
+          <span className="absolute block w-1 h-1 bg-white rounded-full scrolldown__mouse__wheel top-2 left-1/2 -translate-x-1/2 animate-mouse-wheel" />
+        </span>
+      </a>
+    </NextLink>
   </div>
 );
 
