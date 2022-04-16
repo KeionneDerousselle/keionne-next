e43f4eba560d0e27526e4077c8c7b0f014c4835b
Keionne Derousselle <knnderousselle@gmail.com>
2022-04-16 11:55:23 -0600

    using text loop on main page

--- src/components/organisms/intro/index.tsx
+++ src/components/organisms/intro/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import classNames from 'classnames';
 import { Parallax } from './parallax';
 import { AnimatedScrollIndicator } from './animated-scroll-indicator';
+import TextLoop from 'react-text-loop';
 
 export type IntroProps = {
   id: string;
@@ -19,6 +20,16 @@ export const Intro: FC<IntroProps> = ({ id, className }) => (
     <div className="k-container">
       <div className="relative z-20 max-w-xl m-auto text-center">
         <h1 className="mb-2 text-3xl font-bold text-white">Keionne Derousselle</h1>
+        <h2 className="text-lg font-bold text-white">
+          I'm a{' '}
+          <TextLoop springConfig={{ stiffness: 180, damping: 15 }}>
+            <span>senior software engineer</span>
+            <span>full stack engineer</span>
+            <span>product driven engineer</span>
+            <span>quality focused engineer</span>
+            <span>diligent problem solver</span>
+          </TextLoop>
+        </h2>
       </div>
       <AnimatedScrollIndicator href="/#about" />
       <Parallax />
