08af70ede8e5ef7e22fd90083c6d771e4f148846
Keionne Derousselle <knnderousselle@gmail.com>
2022-05-19 10:54:08 -0600

    adding experience timeline animations

--- src/components/molecules/timeline/item/index.tsx
+++ src/components/molecules/timeline/item/index.tsx
@@ -24,24 +24,27 @@ const timelineItemColors = {
     textColor: 'text-white',
     rightArrowColor: 'before:border-r-blue-300',
     leftArrowColor: 'md:before:border-l-blue-300',
+    dotColor: 'after:bg-blue-300',
   },
   yellow: {
     contentBgColor: 'bg-yellow-400',
     textColor: 'text-purple-600',
     rightArrowColor: 'before:border-r-yellow-400',
     leftArrowColor: 'md:before:border-l-yellow-400',
+    dotColor: 'after:bg-yellow-400',
   },
   pink: {
     contentBgColor: 'bg-pink-200',
     textColor: 'text-white',
     rightArrowColor: 'before:border-r-pink-200',
     leftArrowColor: 'md:before:border-l-pink-200',
+    dotColor: 'after:bg-pink-200',
   },
 };
 
 export const TimelineItem: FC<TimelineItemProps> = ({ id, date, title, subtitle, right, color, content }) => {
   const { ref, inView } = useInView({ triggerOnce: true });
-  const { contentBgColor, textColor, rightArrowColor, leftArrowColor } = timelineItemColors[color];
+  const { contentBgColor, textColor, rightArrowColor, leftArrowColor, dotColor } = timelineItemColors[color];
   const formattedDate =
     typeof date === 'string' ? date : date.toLocaleString('en-US', { month: 'short', year: 'numeric' });
 
@@ -51,26 +54,42 @@ export const TimelineItem: FC<TimelineItemProps> = ({ id, date, title, subtitle,
       id={id}
       className={classNames(
         // content
-        'relative opacity-0 left-0 pl-14 py-3 w-full md:w-1/2 transition-all duration-500 ease-in-out',
-        // dot
-        'after:absolute after:w-5 after:h-5 after:rounded-full after:z-10 after:top-5 after:left-0 after:bg-pink-200',
-        //arrow
-        'before:absolute before:w-0 before:h-0 before:border-l-0 before:border-t-[20px] before:border-t-transparent before:border-b-[20px] before:border-b-transparent before:border-r-[20px] before:left-9 before:top-[15%] before:translate-x-0',
-        rightArrowColor,
+        'relative left-0 pl-14 py-3 w-full md:w-1/2',
 
-        // animations on scroll
-        {
-          'opacity-100 transform-none': inView,
-          'transform-gpu translate-x-56': !inView,
-          'md:-translate-x-56': !inView && !right,
-        },
+        // dot
+        'after:absolute after:w-5 after:h-5 after:rounded-full after:z-10 after:top-5 after:left-0 after:transform-gpu after:transition-all after:duration-500 after:ease-in-out',
+        dotColor,
+        inView ? 'after:opacity-100 after:scale-125' : 'after:opacity-0 after:scale-0',
 
         right
           ? 'md:left-1/2 md:pl-14 md:after:-left-3'
-          : `md:left-0 md:pl-0 md:pr-14 md:after:-right-2 md:after:left-auto md:border-r-0 md:before:border-t-[20px] md:before:border-t-transparent md:before:border-r-transparent md:before:border-b-[20px] md:before:border-b-transparent md:before:border-l-[20px] md:before:right-4 md:before:left-auto md:before:top-[15%] md:before:translate-x-0 ${leftArrowColor}`
+          : 'md:left-0 md:pl-0 md:pr-14 md:after:-right-2 md:after:left-auto'
       )}
     >
-      <div className={classNames(contentBgColor, textColor, 'relative p-7 rounded-3xl')}>
+      <div
+        className={classNames(
+          // content
+          'relative p-7 rounded-3xl opacity-0 transition-all duration-500 ease-in-out',
+          contentBgColor,
+          textColor,
+
+          //arrow
+          'before:absolute before:w-0 before:h-0 before:border-l-0 before:border-t-[15px] before:border-t-transparent before:border-b-[15px] before:border-b-transparent before:border-r-[15px] before:-left-3 before:top-[15%] before:translate-x-0',
+          rightArrowColor,
+          {
+            'md:before:border-t-[15px] md:before:border-t-transparent md:before:border-r-transparent md:before:border-b-[15px] md:before:border-b-transparent md:before:border-l-[15px] md:before:-right-7 md:before:left-auto md:before:top-[15%] md:before:translate-x-0':
+              !right,
+            [leftArrowColor]: !right,
+          },
+
+          // animations on scroll
+          {
+            'opacity-100 transform-none': inView,
+            'transform-gpu translate-x-56': !inView,
+            'md:-translate-x-56': !inView && !right,
+          }
+        )}
+      >
         <time className="block mb-1 text-sm">{formattedDate}</time>
         <h3 className="mb-1 text-xl font-bold">{title}</h3>
         <h4 className="mb-3 text-lg font-medium">{subtitle}</h4>
